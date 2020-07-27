c70a142e4bf0d03caa9bfd270493ce0d1e97b8c7
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-07-27 10:49:15 -0300

    Componente com Propriedade

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
-
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro
+        titulo="Situação do Aluno"
+        aluno="Pedro Silva " nota= {9.3} />
+        <ComParametro
+        titulo="Situação do Aluno"
+        aluno="Maria " nota= {9.9} />
     </div>,
 document.getElementById('root')
 )
