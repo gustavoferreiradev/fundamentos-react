3631d6ba5dec8240961cf30b943545e661f3148b
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-07-27 16:17:37 -0300

    Organizados os exercícios em cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
-
+import Card from './components/layout/Card';
 export default _ =>
 
     <div id="app">
         <h1>Fundamentos React</h1>
+        <Card titulo ="#04 - Desafio Aleatório">
         <Aleatorio min = {1} max={60}/>
-        <Fragmento />
+        </Card>
+
+        <Card titulo ="#03 - Fragmento">
+        <Fragmento/>
+        </Card>
+
+        <Card titulo ="#02 - Com Parâmetro">
         <ComParametro
             titulo="Situação do Aluno"
             aluno="Pedro Silva "
             nota={9.3}
         />
+        </Card>
+
+        <Card titulo ="#01 - Primeiro Componente">
         <Primeiro></Primeiro>
+        </Card>
+
+        
+
     </div>
 
