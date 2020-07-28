87eff34cf05791c342025f77801c60c6796ed5ea
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-07-28 17:09:03 -0300

    Renderização Condicional 1

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import ListaAlunos from "./components/repeticao/ListaAlunos";
 import TabelaProdutos from "./components/repeticao/TabelaProdutos";
+import ParOuImpar from "./components/condicional/ParOuImpar";
 
 export default _ =>
 
@@ -16,20 +17,24 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
-        
-        <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
-            <TabelaProdutos></TabelaProdutos>
+
+            <Card titulo="#08 - Renderização Condicional" color="#982395">
+                <ParOuImpar numero={20}></ParOuImpar>
             </Card>
 
-        <Card titulo="#06 - Repetição" color="#FF4C65">
-            <ListaAlunos></ListaAlunos>
+            <Card titulo="#07 - Desafio Repetição" color="#3A9AD9">
+                <TabelaProdutos></TabelaProdutos>
+            </Card>
+
+            <Card titulo="#06 - Repetição" color="#FF4C65">
+                <ListaAlunos></ListaAlunos>
             </Card>
 
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
                 <Familia sobrenome="Silva">
-                <FamiliaMembro nome="Pedro" />
-                <FamiliaMembro nome="Ana" />
-                <FamiliaMembro nome="Gustavo"/>
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" />
                 </Familia>
             </Card>
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
