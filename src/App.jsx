b452de263bd41dacd10903c23b4eb98653fb7d31
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-07-28 13:59:31 -0300

    Repetição

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 
 export default _ =>
 
@@ -15,13 +16,16 @@ export default _ =>
 
         <div className="Cards">
 
+        <Card titulo="#06 - Repetição" color="#FF4C65">
+            <ListaAlunos></ListaAlunos>
+            </Card>
+
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
                 <Familia sobrenome="Silva">
                 <FamiliaMembro nome="Pedro" />
                 <FamiliaMembro nome="Ana" />
                 <FamiliaMembro nome="Gustavo"/>
                 </Familia>
-
             </Card>
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60} />
