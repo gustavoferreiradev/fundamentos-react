6bc0b5bd75935f794de7c03ba1322759a0b126cb
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-07-28 11:31:39 -0300

    Componente com filho 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
 export default _ =>
 
@@ -15,7 +16,12 @@ export default _ =>
         <div className="Cards">
 
             <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
-                <Familia sobrenome="Ferreira"></Familia>
+                <Familia sobrenome="Silva">
+                <FamiliaMembro nome="Pedro" />
+                <FamiliaMembro nome="Ana" />
+                <FamiliaMembro nome="Gustavo"/>
+                </Familia>
+
             </Card>
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={1} max={60} />
