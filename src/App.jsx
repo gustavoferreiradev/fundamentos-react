cf618aa7347d40b299599aee8ba09ab582c9a3ac
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-07-30 10:10:17 -0300

    Comunicação Direta

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ListaAlunos from "./components/repeticao/ListaAlunos";
 import TabelaProdutos from "./components/repeticao/TabelaProdutos";
 import ParOuImpar from "./components/condicional/ParOuImpar";
 import UsuarioInfo from "./components/condicional/UsuarioInfo";
+import DiretaPai from "./components/comunicacao/DiretaPai";
 
 export default _ =>
 
@@ -18,6 +19,11 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+            <Card titulo="#09 - Comunicação Direta" color="#59323C">
+            <DiretaPai></DiretaPai>        
+                           
+            </Card>
 
             <Card titulo="#08 - Renderização Condicional" color="#982395">
                 <ParOuImpar numero={20}></ParOuImpar>
