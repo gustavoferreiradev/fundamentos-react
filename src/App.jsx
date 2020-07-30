947686b3c39310112fd4e9e4c5569846177173ce
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-07-30 11:07:06 -0300

    Comunicação Indireta entre componentes

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import TabelaProdutos from "./components/repeticao/TabelaProdutos";
 import ParOuImpar from "./components/condicional/ParOuImpar";
 import UsuarioInfo from "./components/condicional/UsuarioInfo";
 import DiretaPai from "./components/comunicacao/DiretaPai";
+import IndiretaPai from "./components/comunicacao/IndiretaPai";
 
 export default _ =>
 
@@ -19,6 +20,11 @@ export default _ =>
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+
+        <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
+            <IndiretaPai></IndiretaPai>        
+                           
+            </Card>
 
             <Card titulo="#09 - Comunicação Direta" color="#59323C">
             <DiretaPai></DiretaPai>        
