18019d8820ac956184440aa1d6badcb45a11419c
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2020-08-06 15:05:32 -0300

    Finalizado o contador

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import UsuarioInfo from "./components/condicional/UsuarioInfo";
 import DiretaPai from "./components/comunicacao/DiretaPai";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
 import Input from "./components/formulario/Input";
+import Contador from "./components/contador/Contador"
 
 export default _ =>
 
@@ -22,7 +23,10 @@ export default _ =>
 
         <div className="Cards">
 
-        
+        <Card titulo="#12 - Contador" color="#424242">
+            <Contador numeroInicial={10}></Contador>     
+            </Card>
+
         <Card titulo="#11 - Componente Controlado (Input)" color="#E45F56">
             <Input></Input>        
                            
